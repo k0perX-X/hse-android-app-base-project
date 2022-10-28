3741a9bc3fad002bfb0308fa0872670cfbb47542
Николай Перминов <kolya-perminov@ya.ru>
2022-10-28 22:54:24 +0500

    Kotlin yeah

--- app/src/main/java/org/hse/baseproject/MainActivity.kt
+++ app/src/main/java/org/hse/baseproject/MainActivity.kt
@@ -1,49 +1,31 @@
-package org.hse.baseproject;
+package org.hse.baseproject
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        View button_for_stud = findViewById(R.id.button_for_stud);
-        View button_for_prep = findViewById(R.id.button_for_prep);
-
-        button_for_stud.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clickButtonForStud();
-            }
-        });
-        button_for_prep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clickButtonForPrep();
-            }
-        });
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button_for_stud = findViewById<View>(R.id.button_for_stud)
+        val button_for_prep = findViewById<View>(R.id.button_for_prep)
+        button_for_stud.setOnClickListener { clickButtonForStud() }
+        button_for_prep.setOnClickListener { clickButtonForPrep() }
     }
 
-    private void clickButtonForStud(){
-        CharSequence text = getString(R.string.toast_rasp_for_stud);
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-        toast.show();
+    private fun clickButtonForStud() {
+        val text: CharSequence = getString(R.string.toast_rasp_for_stud)
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
     }
 
-    private void clickButtonForPrep(){
-        CharSequence text = getString(R.string.toast_rasp_for_prep);
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-        toast.show();
-
+    private fun clickButtonForPrep() {
+        val text: CharSequence = getString(R.string.toast_rasp_for_prep)
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
     }
 }
